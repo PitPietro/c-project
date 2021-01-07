54d21a670daa3130d95e8e42f3a76cd09676fce2
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-01-07 17:47:05 +0100

    tail recursive factorial

--- recursion/math-es/factorial.c
+++ recursion/math-es/factorial.c
@@ -1,23 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int factorial(int n) {
+long factorial(int n) {
     // exit condition
     if(n == 0) {
         return 1;
     } else {
-        return n + factorial(n-1);
+        return n * factorial(n-1);
     }
 }
 
-int main() {
-    int a = 10;
-    int result = factorial(a);
 
-    printf("The factorial of %d is %d\n", a, result);
+long tail_fact(int n) {
+    long res = 1;
+    int i = 1;
+
+    while(i <= n) {
+        res *= i;
+        i++;
+    }
+
+    return res;
+}
+
+int main() {
+    int a;   
+
+    printf("Insert the number you want to evaluate the factorial of: ");
+    scanf("%d", &a);
+
+    long result = factorial(a);
+    long tailRes = tail_fact(a);
+
+    printf("The factorial of %d is %ld\n", a, result);
+    printf("The factorial of %d is %ld\n", a, tailRes);
 
     return 0;
 }
 
 // cd recursion/math-es
 // gcc -o fact factorial.c && ./fact
+
+/*
+Evaluates the factorial of a given number.
+The return type of the funcions is 'long' since the result could be very huge.
+While the user input is an int, since it could be very difficult to evaluate
+the factorial of a 'long' number.
+*/

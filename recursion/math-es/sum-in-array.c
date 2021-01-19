f72bd25c870e6a30eabcd61a4713ad5d0e0f87d3
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-01-19 18:22:04 +0100

    sum in array of integers - fix recursive way

--- recursion/math-es/sum-in-array.c
+++ recursion/math-es/sum-in-array.c
@@ -3,31 +3,31 @@
 
 #define MAX 10
 
-/*
-int recursive_sum_array(int numbers[], int first, int last) {
-    if(first == last) {
-        return numbers[last];
+
+int recursive_sum_array(int numbers[], int first) {
+    // if(first == MAX) is wrong since number[MAX] does not exist
+    if(first == MAX-1) {
+        return numbers[first];
     } else {
-        return numbers[first] + recursive_sum_array(numbers, first+1, last);
+        return numbers[first] + recursive_sum_array(numbers, first+1);
     }
 }
-*/
 
-int iterative_sum_v0(int numbers[], int size) {
+int iterative_sum_array_v0(int numbers[]) {
     int i, result = 0;
 
-    for(i = 0; i < size; i++) {
+    for(i = 0; i < MAX; i++) {
         result += numbers[i];
     }
 
     return result;
 }
 
-int iterative_sum(int numbers[], int size) {
+int iterative_sum_array(int numbers[]) {
     int i = 0;
     int result = 0;
 
-    while(i < size) {
+    while(i < MAX) {
         result += numbers[i];
         i++;
     }
@@ -35,15 +35,15 @@ int iterative_sum(int numbers[], int size) {
     return result;
 }
 
-int tail_sum(int numbers[], int size, int iterator, int result) {
+int tail_sum_array(int numbers[], int iterator, int result) {
     int i = iterator;
     int res = result;
 
-    while(i < size) {
+    while(i < MAX) {
         res += numbers[i];
         i++;
 
-        return tail_sum(numbers, size, i, res);
+        return tail_sum_array(numbers, i, res);
     }
 
     return res;
@@ -52,12 +52,12 @@ int tail_sum(int numbers[], int size, int iterator, int result) {
 int main() {
     int array[MAX] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    // int recSum = recursive_sum_array(array, 0, MAX);
-    int iterSumFor = iterative_sum_v0(array, MAX);
-    int iterSum = iterative_sum(array, MAX);
-    int tailSum = tail_sum(array, MAX, 0, 0);
+    int recSum = recursive_sum_array(array, 0);
+    int iterSumFor = iterative_sum_array_v0(array);
+    int iterSum = iterative_sum_array(array);
+    int tailSum = tail_sum_array(array, 0, 0);
 
-    // printf("%d\t recursive\n", recSum);
+    printf("%d\t recursive\n", recSum);
     printf("%d\t iterative for-loop\n", iterSumFor);
     printf("%d\t iterative while-loop\n", iterSum);
     printf("%d\t tail recursive\n", tailSum);

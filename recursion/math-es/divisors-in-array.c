4dd665762191f4c844535e309116799277363d86
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-01-20 09:15:44 +0100

    find divisors iterative and tail

--- recursion/math-es/divisors-in-array.c
+++ recursion/math-es/divisors-in-array.c
@@ -14,32 +14,64 @@ int recursive_divisor_array(int numbers[], int div, int last) {
     }
 }
 
-int iterative_divisor_array_v0(int numbers[]) {
-    // ...
+int iterative_divisor_array_v0(int numbers[], int div, int last) {
+    int i;
+    int res = 0;
+
+    for(i = 0; i <= last; i++) {
+        if(numbers[i] % div == 0) {
+            res += 1;
+        }
+    }
+
+    return res;
 }
 
-int iterative_divisor_array(int numbers[]) {
-    // ...
+int iterative_divisor_array(int numbers[], int div, int last) {
+    int i = 0;
+    int res = 0;
+
+    while(i <= last) {
+        if(numbers[i] % div == 0) {
+            res += 1;
+        }
+
+        i++;
+    }
+
+    return res;
 }
 
-int tail_divisor_array(int numbers[], int iterator, int result) {
-    // ...
+int tail_divisor_array(int numbers[], int div, int last, int iterator, int result) {
+    int i = iterator;
+    int res = result;
+
+    if(i <= last) {
+        if(numbers[i] % div == 0) {
+            res += 1;
+        }
+        i++;
+
+        return tail_divisor_array(numbers, div, last, i, res);
+    }
+
+    return res;
 }
 
 int main() {
     int array[MAX] = {27, 9, 3, 4, 5, 6, 7, 8, 9, 10};
+    int d = 3;
+    int l = 5;
 
-    int recDiv = recursive_divisor_array(array, 3, 5);
-    int iterDivFor = iterative_divisor_array_v0(array);
-    int iterDiv = iterative_divisor_array(array);
-    int tailDiv = tail_divisor_array(array, 0, 0);
+    int recDiv = recursive_divisor_array(array, d, l);
+    int iterDivFor = iterative_divisor_array_v0(array, d, l);
+    int iterDiv = iterative_divisor_array(array, d, l);
+    int tailDiv = tail_divisor_array(array, d, l, 0, 0);
 
     printf("%d\t recursive\n", recDiv);
-    /*
     printf("%d\t iterative for-loop\n", iterDivFor);
     printf("%d\t iterative while-loop\n", iterDiv);
     printf("%d\t tail recursive\n", tailDiv);
-    */
     return 0;
 }
 

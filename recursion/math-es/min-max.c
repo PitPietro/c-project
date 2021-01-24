70763824dda1327f36e56f9dfa2267b21789b924
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-01-24 21:21:39 +0100

    find min recursive in array

--- recursion/math-es/min-max.c
+++ recursion/math-es/min-max.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void min(int numbers[], int index, int * m) {
+    if(index < 0) {
+        return;
+    } else {
+        if(*m > numbers[index]) {
+            *m = numbers[index];
+        }
+
+        printf("|  i = %d  |  n[%d] = %d\t|  m = %d  |\n", index, index, numbers[index], *m);
+
+        min(numbers, index - 1, m);
+    }
+}
+
 void max(int numbers[], int index, int * m) {
     if(index < 0) {
         return;
@@ -22,12 +36,14 @@ int main() {
 
     int size = sizeArr / sizeArrEl;
     printf("size = %ld / %ld = %d\n", sizeArr, sizeArrEl, size);
-    
-    int M = arr[size-1];
-    printf("Last element of the array is %d at index %d\n\n", M, size-1);
 
-    max(arr, size-2, &M);
-    printf("\n~ ~ L 0 a d 1 n g ~ ~\n\nmax = %d\n", M);
+    int minArray = arr[size-1];
+    min(arr, size-2, &minArray);
+    printf("\n~ ~ L 0 a d 1 n g ~ ~\n\nmin = %d\n\n", minArray);
+    
+    int maxArray = arr[size-1];
+    max(arr, size-2, &maxArray);
+    printf("\n~ ~ L 0 a d 1 n g ~ ~\n\nmax = %d\n\n", maxArray);
 }
 
 // cd recursion/math-es

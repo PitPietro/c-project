0b5a1bf35aaa533995bff85526c0f73c7c768c3b
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-06-07 08:11:29 +0200

    open_from_file() returns a string

--- data-types/adt/linked-list/list-of-struct/actions-over-files/address-book/main.c
+++ data-types/adt/linked-list/list-of-struct/actions-over-files/address-book/main.c
@@ -5,16 +5,27 @@
 
 void menu(void);
 
+root open_from_file();
+
 int main() {
     int choose;
     list root = NULL;
-    
+
+    printf("\nADT ~ List");
+
+    char *file = open_from_file();
+    if (strcmp(file, "")) {
+        printf("Open from %s file", file);
+    } else {
+        printf("Empty string");
+    }
+
     do {
         menu();
         printf("\nChoose: ");
-		scanf("%d", &choose);
-        
-        switch(choose) {
+        scanf("%d", &choose);
+
+        switch (choose) {
             case 1: {
                 root = insert_address_book(root);
                 break;
@@ -38,8 +49,8 @@ int main() {
             default:
                 printf("Try again, you will be luckier!\n");
         }
-    } while(choose != 0);
-    
+    } while (choose != 0);
+
     return 0;
 }
 
@@ -50,6 +61,23 @@ void menu(void) {
     printf("  3) ~~ Search\n");
     printf("  4) ~~ Print Elements\n");
     printf("  0) ~~ Exit\n");
+}
+
+char *open_from_file() {
+    printf("\nLoad address book from file? (Y/n): ");
+    char c;
+    scanf("%c", &c);
+
+    if ((c == 'Y') || (c == 'y')) {
+        printf("Insert file name: ");
+        char *fileName;
+        scanf("%s", fileName);
+
+        printf("File name: %s", fileName);
+        return fileName;
+    } else {
+        return "";
+    }
 }
 
 // cd data-types/adt/linked-list/list-of-struct/actions-over-files/address-book/
